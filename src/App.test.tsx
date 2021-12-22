48c83e84f0c7857ad6a021c02d98cf0580879ee8
Gerald Isabelle <info@geraldisabelle.me>
2021-12-22 10:46:06 +0000

    add project to git

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,83 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, waitFor, fireEvent } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const setup = () => {
+  const component = render(<App />)
+
+  return {
+    ...component,
+  }
+}
+
+describe('Capsule', () => {
+  it('should render list of capsules', async () => {
+    const mockFetchPromise = Promise.resolve({
+      json: () => [
+        {
+          capsule_serial: 'The first element',
+          details: 'details first element',
+        },
+      ],
+    })
+    window.fetch = jest.fn().mockImplementation(() => mockFetchPromise)
+
+    const { getByText } = setup()
+
+    await waitFor(() => {
+      getByText('The first element')
+    })
+  })
+
+  it('should error', async () => {
+    const mockFetchPromise = Promise.reject({ err: '404' })
+    window.fetch = jest.fn().mockImplementation(() => mockFetchPromise)
+
+    const { getByText } = setup()
+
+    await waitFor(() => {
+      getByText('an error has occurred')
+    })
+  })
+
+  it('should display details', async () => {
+    const mockFetchPromise = Promise.resolve({
+      json: () => [
+        {
+          capsule_serial: 'The first element',
+          details: 'details first element',
+        },
+      ],
+    })
+    window.fetch = jest.fn().mockImplementation(() => mockFetchPromise)
+
+    const { getByText } = setup()
+
+    await waitFor(() => fireEvent.click(getByText('find out more')))
+
+    await waitFor(() => {
+      getByText('details first element')
+    })
+  })
+
+  it('should back button', async () => {
+    const mockFetchPromise = Promise.resolve({
+      json: () => [
+        {
+          capsule_serial: 'The first element',
+          details: 'details first element',
+        },
+      ],
+    })
+    window.fetch = jest.fn().mockImplementation(() => mockFetchPromise)
+
+    const { getByText } = setup()
+
+    await waitFor(() => fireEvent.click(getByText('find out more')))
+    await waitFor(() => fireEvent.click(getByText('back')))
+
+    await waitFor(() => {
+      getByText('The first element')
+    })
+  })
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface mission {
+  name: string
+  flight: number
 }
 
-export default App;
+interface rocket {
+  capsule_serial: string
+  capsule_id: string
+  status: string
+  original_launch: string
+  original_launch_unix: number
+  missions: mission[]
+  landings: number
+  type: string
+  details: string
+  reuse_count: number
+}
+
+function App() {
+  const [rockets, setRockets] = useState<rocket[]>([])
+  const [details, setDetails] = useState<rocket | null>(null)
+  const [error, setError] = useState(false)
+  const fetchRockets = async () => {
+    try {
+      const response = await fetch('https://api.spacexdata.com/v3/capsules')
+      const rockets: rocket[] = await response.json()
+      setRockets(rockets)
+    } catch (e) {
+      setError(true)
+      console.error(e)
+    }
+  }
+
+  const detailView = (id: string) => {
+    const data = rockets.find((x) => x.capsule_serial === id)
+    if (data) setDetails(data)
+  }
+
+  useEffect(() => {
+    fetchRockets()
+  }, [])
+
+  return (
+    <div className='App'>
+      {details ? (
+        <>
+          <p>{details.details}</p>
+          <button onClick={() => setDetails(null)}>back</button>
+        </>
+      ) : (
+        <>
+          {rockets.map(({ capsule_serial, capsule_id }) => {
+            return (
+              <div key={capsule_serial}>
+                <div>{capsule_serial}</div>
+                <button onClick={() => detailView(capsule_serial)}>
+                  find out more
+                </button>
+              </div>
+            )
+          })}
+        </>
+      )}
+      {error && <p>an error has occurred</p>}
+    </div>
+  )
+}
+
+export default App
